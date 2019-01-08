f091661260564744350fe828fcd2b38b94bd7858
Yacine <taalba@hotmail.com>
2019-01-08 12:59:45 -0500

    Add project files.

---
+++ Neo4jApp/runserver.py
@@ -0,0 +1,14 @@
+"""
+This script runs the Neo4jApp application using a development server.
+"""
+
+from os import environ
+from Neo4jApp import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
